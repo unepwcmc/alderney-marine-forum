06d2c89bd54698cb7a66234f67fe8cd7b9d29374
Spencer Dixon <spencerlloyddixon@gmail.com>
2016-11-28 16:23:15 +0000

    Another config

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,9 +8,8 @@ CarrierWave.configure do |config|
     #region:                 'us-east-1',
     aws_access_key_id:      ENV["AWS_ACCESS_KEY"],
     aws_secret_access_key:  ENV["AWS_SECRET_KEY"],
-    endpoint:               'http://s3.amazonaws.com/alderney.cms.images'             # optional, defaults to nil
   }
-  config.fog_directory  = ''
+  config.fog_directory  = 'alderney.cms.images'
   config.fog_public     = true
   config.fog_attributes = { 'Cache-Control' => 'max-age=315576000' }
 
