d53babd579a821662a9a34bcfe517996a0adf4bf
Stuart Watson <stuart.watson@unep-wcmc.org>
2022-02-02 14:42:05 +0000

    Added updated recaptcha initializer

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,6 @@
 Recaptcha.configure do |config|
-  config.site_key  = 'GOOGLE_SITE_KEY'
-  config.secret_key = 'GOOGLE_SECRET_KEY'
+  config.site_key  = ENV["GOOGLE_SITE_KEY"]
+  config.secret_key = ENV["GOOGLE_SECRET_KEY"]
 end
+
+
