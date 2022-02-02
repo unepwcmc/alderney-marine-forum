8fbf402882ec30e902eac3a9615d4b803924a798
Stuart Watson <stuart.watson@unep-wcmc.org>
2022-02-02 14:02:03 +0000

    Updated config

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,4 +1,4 @@
 Recaptcha.configure do |config|
-  config.public_key  = 'GOOGLE_SITE_KEY'
-  config.private_key = 'GOOGLE_SECRET_KEY'
+  config.site_key  = 'GOOGLE_SITE_KEY'
+  config.secret_key = 'GOOGLE_SECRET_KEY'
 end
